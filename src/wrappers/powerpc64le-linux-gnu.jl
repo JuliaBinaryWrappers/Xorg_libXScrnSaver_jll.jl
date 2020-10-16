a8940f400e284f4e96afd7a8f3248e7e3088a8c0
jlbuild <juliabuildbot@gmail.com>
2020-10-16 22:20:30 +0000

    Xorg_libXScrnSaver_jll build 1.2.3+3

--- src/wrappers/powerpc64le-linux-gnu.jl
+++ src/wrappers/powerpc64le-linux-gnu.jl
@@ -2,57 +2,15 @@
 export libXScrnSaver
 
 using Xorg_libXext_jll
-## Global variables
-PATH = ""
-LIBPATH = ""
-LIBPATH_env = "LD_LIBRARY_PATH"
-
-# Relative path to `libXScrnSaver`
-const libXScrnSaver_splitpath = ["lib", "libXss.so"]
-
-# This will be filled out by __init__() for all products, as it must be done at runtime
-libXScrnSaver_path = ""
-
-# libXScrnSaver-specific global declaration
-# This will be filled out by __init__()
-libXScrnSaver_handle = C_NULL
-
-# This must be `const` so that we can use it with `ccall()`
-const libXScrnSaver = "libXss.so.1"
-
-
-"""
-Open all libraries
-"""
+JLLWrappers.@generate_wrapper_header("Xorg_libXScrnSaver")
+JLLWrappers.@declare_library_product(libXScrnSaver, "libXss.so.1")
 function __init__()
-    global artifact_dir = abspath(artifact"Xorg_libXScrnSaver")
+    JLLWrappers.@generate_init_header(Xorg_libXext_jll)
+    JLLWrappers.@init_library_product(
+        libXScrnSaver,
+        "lib/libXss.so",
+        RTLD_LAZY | RTLD_DEEPBIND,
+    )
 
-    # Initialize PATH and LIBPATH environment variable listings
-    global PATH_list, LIBPATH_list
-    # We first need to add to LIBPATH_list the libraries provided by Julia
-    append!(LIBPATH_list, [joinpath(Sys.BINDIR, Base.LIBDIR, "julia"), joinpath(Sys.BINDIR, Base.LIBDIR)])
-    # From the list of our dependencies, generate a tuple of all the PATH and LIBPATH lists,
-    # then append them to our own.
-    foreach(p -> append!(PATH_list, p), (Xorg_libXext_jll.PATH_list,))
-    foreach(p -> append!(LIBPATH_list, p), (Xorg_libXext_jll.LIBPATH_list,))
-
-    global libXScrnSaver_path = normpath(joinpath(artifact_dir, libXScrnSaver_splitpath...))
-
-    # Manually `dlopen()` this right now so that future invocations
-    # of `ccall` with its `SONAME` will find this path immediately.
-    global libXScrnSaver_handle = dlopen(libXScrnSaver_path)
-    push!(LIBPATH_list, dirname(libXScrnSaver_path))
-
-    # Filter out duplicate and empty entries in our PATH and LIBPATH entries
-    filter!(!isempty, unique!(PATH_list))
-    filter!(!isempty, unique!(LIBPATH_list))
-    global PATH = join(PATH_list, ':')
-    global LIBPATH = join(LIBPATH_list, ':')
-
-    # Add each element of LIBPATH to our DL_LOAD_PATH (necessary on platforms
-    # that don't honor our "already opened" trick)
-    #for lp in LIBPATH_list
-    #    push!(DL_LOAD_PATH, lp)
-    #end
+    JLLWrappers.@generate_init_footer()
 end  # __init__()
-
